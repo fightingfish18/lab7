e4b263b8719ea04d117959aee98448142dc5d4e7
William Smyth May <wsmay1@uw.edu>
2013-11-10 21:14:00 -0800

    Finalized and ready to turn in

--- js/earthquakes.js
+++ js/earthquakes.js
@@ -1,3 +1,8 @@
+//William Smyth May
+//2013-11-11
+//This is JavaScript file for lab 7
+//creates and populates the google map on the earthquake page
+
 /* earthquakes.js
     Script file for the INFO 343 Lab 7 Earthquake plotting page
 
@@ -29,4 +34,52 @@ gov.usgs.quakesMap;
 $(document).ajaxError(function(event, jqXHR, err){
     alert('Problem obtaining data: ' + jqXHR.statusText);
 });
+
+//Window.onload function
+$(function() {
+	$('.message').html('Loading... <img src="img/loading.gif">'); //Loading icon while JSON request is made
+	getQuakes(); //calls function for obtaining JSON and creating the map
+});
+
+//This function makes and processes the JSON request
+function getQuakes() {
+	$.getJSON(gov.usgs.quakesUrl, function(quakes) {
+		$('.message').html('Displaying ' + quakes.length + ' earthquakes');
+		gov.usgs.quakesMap = new google.maps.Map($('.map-container')[0], {
+			center: new google.maps.LatLng(0,0),
+			zoom: 2,
+			mapTypeId: google.maps.MapTypeId.TERRAIN,
+			streetViewControl: false
+		});
+		addQuakeMarkers(quakes, gov.usgs.quakesMap); //adds markers for each quake
+	});
+	
+//This function adds markers to the Google maps object
+function addQuakeMarkers(quakes, map) {
+	for (var i = 0; i < quakes.length; i++) {
+		var quake = quakes[i];
+		if (quake.location) { //Verifies that latitude and longitude are provided for the given quake
+			quake.mapMarker = new google.maps.Marker({
+			map: map,
+			position: new google.maps.LatLng(quake.location.latitude, quake.location.longitude)
+			});
+		}
+		var infoWindow = new google.maps.InfoWindow({ //Info window for users to click
+			content: new Date(quake.datetime).toLocaleString() + ': magnitude ' + quake.magnitude + ' at depth of ' + quake.depth + ' meters'
+		});
+		registerInfoWindow(map, quake.mapMarker, infoWindow); //applies the info window to each marker
+	}
+}
+
+//Opens the info window when the marker is clicked
+function registerInfoWindow(map, marker, infoWindow) {
+	google.maps.event.addListener(marker, 'click', function() {
+		if (gov.usgs.iw) { //if another window is open, it will be closed.
+			gov.usgs.iw.close();
+		}
+		gov.usgs.iw = infoWindow;
+		infoWindow.open(map, marker);
+	});
+}
+		
 
